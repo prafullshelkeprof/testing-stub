8ace9e150538ff92e6b5a73184e5f805e014c36c
Prafull Shelke <prafull.shelke@ness.com>
2016-01-12 12:01:52 +0530

    update code and run first app

--- angular-2-quickstart/scripts/app/app.component.ts
+++ angular-2-quickstart/scripts/app/app.component.ts
@@ -2,7 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-	templateUrl: 'views/hello-world.html'
-    // template:'<div>this is awesome</div>'
+	templateUrl: 'views/hello-world.html'  
 })
 export class AppComponent { }

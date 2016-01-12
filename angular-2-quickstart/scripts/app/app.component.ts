7e27f522052dfc1f6383573d8219d127569e66a3
Prafull Shelke <prafull.shelke@ness.com>
2016-01-12 16:28:33 +0530

    Implement two way data binding feature with angular 2

--- angular-2-quickstart/scripts/app/app.component.ts
+++ angular-2-quickstart/scripts/app/app.component.ts
@@ -5,13 +5,13 @@ interface Hero {
 }
 @Component({
     selector: 'my-app',
-	templateUrl: 'views/tour-of-heroes.html'  
+	templateUrl: 'views/tour-of-heroes.html'
 })
-export class AppComponent { 
-	public title = 'Tour of Heroes';
+export class AppComponent {
+	public title: string = 'Tour of Heroes';
 
-	public hero :Hero = {
-		id:1,
-		name:'Windstorm'
-	}
+	public hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	}	
 }

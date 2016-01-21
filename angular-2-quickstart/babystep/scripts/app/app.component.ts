c5f8e8e75018aaaa5e49db2eac5c27a1c5249277
Prafull Shelke <prafull.shelke@ness.com>
2016-01-21 16:46:49 +0530

    update folder structure for apps

--- angular-2-quickstart/babystep/scripts/app/app.component.ts
+++ angular-2-quickstart/babystep/scripts/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-	templateUrl: 'views/hello-world.html'
-    // template:'<div>this is awesome</div>'
+	//templateUrl: 'views/hello-world.html',
+	template: `<h1>My First Angular 2 App is loaded....</h1>`
 })
 export class AppComponent { }

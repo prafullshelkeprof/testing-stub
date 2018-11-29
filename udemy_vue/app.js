dc6fd34e578966c57798eea36b7be5fe13ea410c
Prafull Shelke <prafull.shelke@morningstar.com>
2018-11-29 12:35:36 +0530

    PShelke: Add Vuejs Hello world app

---
+++ udemy_vue/app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Hello World!',
+        link: 'http://google.com'
+    },
+    methods: {
+        changeTitle: function(event) {
+            this.title = event.target.value;
+        },
+        sayHello: function() {
+            return this.title;
+        }
+    }
+});
